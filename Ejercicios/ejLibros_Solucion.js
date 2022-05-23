fa028c95546b36feffdfcb12f992f8d3876655b1
xavi1978 <exa01@protonmail.ch>
2022-05-23 13:57:25 +0200

    Ejercicios_2022-05-23

--- Ejercicios/ejLibros_Solucion.js
+++ Ejercicios/ejLibros_Solucion.js
@@ -2,7 +2,15 @@
 let listaLibros = [];
 
 class Libro {
-	constructor(sn, titulo, autor, edicion, precio = 0, tematica = []) {
+	constructor(
+		sn,
+		titulo,
+		autor,
+		// edicion = nombreAleatorio(),
+		edicion,
+		precio = 0,
+		tematica = []
+	) {
 		this.sn = sn;
 		this.titulo = titulo;
 		this.autor = autor;
@@ -12,10 +20,28 @@ class Libro {
 		listaLibros.push(this);
 		this.pintarLibrosEnLaTabla();
 	}
+
+	//Libro.newConstructor()
+	static newConstructor(sn, titulo, autor) {
+		new Libro(
+			sn,
+			titulo,
+			autor,
+			nombreAleatorio(),
+			nombreAleatorio(),
+			nombreAleatorio()
+		);
+	}
+
 	pintarLibrosEnLaTabla() {
 		let boton_eliminar = document.createElement("button");
 		boton_eliminar.innerHTML = "Remove";
 		boton_eliminar.value = "libro_" + this.sn;
+		boton_eliminar.addEventListener("click", function () {
+			// document.getElementById(this.value).remove(); //.innerHTML = "";
+			tbody.removeChild(tr);
+			//todo: eliminar de la lista
+		});
 
 		let td_sn = document.createElement("td");
 		let td_titulo = document.createElement("td");
@@ -61,6 +87,31 @@ class Libro {
 		}
 		return acumulado.slice(0, -2);
 	}
+	// console.table
+	// ordenar(
+	pintarEnConsola() {
+		//console.table(this);
+		console.table([
+			[
+				this.sn,
+				this.titulo,
+				this.autor,
+				this.edicion,
+				this.precio,
+				this.pintarLasTematicasFormateadasLibro(),
+			],
+		]);
+	}
+	toJSON() {
+		return JSON.stringify({
+			sn: this.sn,
+			titulo: this.titulo,
+			autor: this.autor,
+			edicion: this.edicion,
+			precio: this.precio,
+			tematica: this.pintarLasTematicasFormateadasLibro(),
+		});
+	}
 }
 
 new Libro("ABC123", "Muse", "canelita", 3, 700, [
@@ -72,3 +123,22 @@ new Libro("BCD234", "JS", "Mascarilla", 18, 0.21, ["romance", "comedia"]);
 new Libro("BCD234", "JS", "Mascarilla", 18);
 console.log(listaLibros);
 console.log(Libro.pintarLasTematicasFormateadas(listaLibros[0].tematica));
+
+let uno = listaLibros[0].toJSON();
+let dos = JSON.parse(uno);
+console.table([dos]);
+console.table([{ x: 1, y: 2 }]);
+
+let objeto1 = { x: 1, y: 2 };
+let objeto2 = { x: 1, y: 3 };
+console.assert(Object.is(objeto1, objeto2));
+
+// "nombre":"valor","campo2"?"valor2";
+// "nombre3":"valor","campo4"?"valor2";
+// "nombre5":"valor","campo6"?"valor2";
+
+// 3.54
+// 3,54
+
+// [a, 12, 5, 4, 1, g, c, B]
+// [1, 4, 5, 12, a, c, g, B]
